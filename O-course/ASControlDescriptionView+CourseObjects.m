ee8fe559b45f0df25a60bec9f50715eaef4caf23
Erik Aderstedt <erik@aderstedt.se>
2013-09-16 12:01:00 +0200

    Åtgärdar saknad symbol i listan.

--- O-course/ASControlDescriptionView+CourseObjects.m
+++ O-course/ASControlDescriptionView+CourseObjects.m
@@ -92,6 +92,7 @@
                       @(kASFeatureStream),
                       @(kASFeatureDitch),
                       @(kASFeatureNarrowMarch),
+                      @(kASFeatureMarch),
                       @(kASFeatureFirmGroundInMarch),
                       @(kASFeatureWell),
                       @(kASFeatureSpring),
