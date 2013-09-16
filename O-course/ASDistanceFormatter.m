fbe00d9e9b604d8a078140b516dcaee737ff8be9
Erik Aderstedt <erik@aderstedt.se>
2013-09-16 11:33:33 +0200

    Åtgärda problem med avståndsformatteringen. Avrunda också till närmaste 10-tal meter.

--- O-course/ASDistanceFormatter.m
+++ O-course/ASDistanceFormatter.m
@@ -19,9 +19,25 @@
     return self;
 }
 
+- (id)init {
+    self = [super init];
+    if (self) {
+        [self setMaximumFractionDigits:1];
+        [self setMinimumFractionDigits:1];
+    }
+    return self;
+}
+
 - (NSString *)stringForObjectValue:(id)obj {
+    if (obj == nil || [obj floatValue] == 0.0) return @"";
+    
     if ([(NSNumber *)obj floatValue] < 1.0 && [(NSNumber *)obj floatValue] > 0.0) {
-        return [NSString stringWithFormat:@"%@ m", [super stringForObjectValue:[NSNumber numberWithFloat:1000.0*[(NSNumber *)obj floatValue]]]];
+        CGFloat f = [(NSNumber *)obj floatValue];
+        f = round(f*100.0);
+        [self setMaximumFractionDigits:0];
+        NSString *s = [NSString stringWithFormat:@"%@ m", [super stringForObjectValue:@(f*10.0)]];
+        [self setMaximumFractionDigits:1];
+        return s;
     }
     return [NSString stringWithFormat:@"%@ km", [super stringForObjectValue:obj]];    
 }
